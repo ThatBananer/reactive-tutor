ceaa6b788150974b8e2e89c9d1f7fd6edb6262b9
Jon Carlos O'Neil <jco103001@gmail.com>
2023-06-20 23:19:48 -0400

    initial commit and added start of userTile component

---
+++ src/components/tutorListing.js
@@ -0,0 +1,23 @@
+function tutorListing() {
+  return <h2>Hi, I am a Car!</h2>;
+}
+
+export default tutorListing;
+
+<div>
+  <a>
+    <div className="tutorPhoto">
+      <img src = "source link in firebase"/>
+    </div>
+  </a>
+  <div class="userInfo">
+    <a class="userInfoLink">
+      <div class = "info">
+        <div id = "tutorBio"> persons NAME</div>
+        <div id = "school">SCHOOL</div>
+        <div id = "desc">Hi, my name is NAME</div>
+      </div>
+    </a>
+  </div>
+
+</div>
