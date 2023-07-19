9d214f2afbbfe6c409fd092f6af354a8cd4f6492
Jon Carlos O'Neil <jco103001@gmail.com>
2023-07-18 23:05:04 -0400

    some css work
    -tried to refactor landing page css
    -Added more styling to dashboard side bar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -11,19 +11,28 @@ function SideBar({ onSelect }) {
       <div className={styles.logoImg}>
         <img src="https://media.licdn.com/dms/image/D4E0BAQGduTIDCuZc5A/company-logo_100_100/0/1686498389952?e=1696464000&v=beta&t=2sukds1qbRlKny3UvUcYddVO3T_CMic_kgiMR8Tzw50" alt="Logo" className={styles.logoImg} />
       </div>
+      <h4>BLANK Dashboard</h4>
       <ul className={styles.navigation}>
         <li className={styles.navigationLi} onClick={() => handleOptionClick('findTutors')}>
           <img src="tutor.png" alt="Find Tutors" className={styles.navIcon} />
           <span>Find Tutors</span>
+        
         </li>
+        <hr></hr>
+
         <li className={styles.navigationLi} onClick={() => handleOptionClick('settings')}>
           <img src="settings.png" alt="Settings" className={styles.navIcon} />
           <span>Settings</span>
+          
         </li>
+        <hr></hr>
+
         <li className={styles.navigationLi} onClick={() => handleOptionClick('about')}>
           <img src="about.png" alt="About Us" className={styles.navIcon} />
           <span>About Us</span>
+         
         </li>
+        <hr></hr>
       </ul>
     </div>
   );
