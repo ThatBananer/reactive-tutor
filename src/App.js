5d248274230e2b75c3b972db0015d94df45da75e
Jon Carlos O'Neil <jco103001@gmail.com>
2023-08-03 23:56:39 -0400

    added requirment of user auth for some pages
    see title

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import styles from './reactiveTutoring.module.css'
-import TutorListingRepeater from './components/TutorListings/TutorListingRepeater'; // This error doesnt seem to be causing real problems
+import TutorListingRepeater from './components/TutorListings/TutorListingRepeater';
 
 import FakeFireBaseQueryResults from './shittyFirebaseSimulation/DefinetlyFireBase';
 import TopBar from './components/TopBar/TopBar';
 import SideBar from './components/SideBar/SideBar';
 
-
 import landingPage from './pages/landingPage/landingPage';
 import loginPage from './pages/loginPage/logInPage';
 import Signup from './pages/signUpPage/signUpPage';
 import mainPage from './pages/mainPage/mainPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ProfileSetup from './pages/profileSetup/ProfileSetup';
 
-//import FakeFireBaseQueryResults from '../shittyFirebaseSimulation/DefinetlyFireBase';
-
 function App() {
+  const currentUser = false;
+
+  const RequireAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="/signup" />;
+  };
+
   return (
     <Router>
       <Routes>
-        <Route exact path='/' Component={landingPage}/>
-        <Route path ='/login' Component={loginPage}/>
-        <Route path ='/signup' Component={Signup}/>
-        <Route path = 'signup/profileSetup' Component={ProfileSetup}/>
-        <Route path = '/main' Component={mainPage}/>
+        <Route exact path="/" element={<landingPage />} />
+        <Route path="/login" element={<loginPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/signup/profileSetup"
+          element={<RequireAuth><ProfileSetup /></RequireAuth>}
+        />
+        <Route path="/main" element={<RequireAuth><mainPage /></RequireAuth>} />
       </Routes>
     </Router>
   );
